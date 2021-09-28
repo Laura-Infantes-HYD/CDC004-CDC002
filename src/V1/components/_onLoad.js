df23a6aeabb982997f3a9210a079154b5e020e8f
Laura <laura@HYD-Admins-MacBook-Pro.local>
2021-09-28 12:29:09 +0200

    initial commit

--- src/V1/components/_onLoad.js
+++
@@ -1,8 +0,0 @@
-import pollForEl from '../../helpers/pollForEl';
-import removeDropdownOnblur from './dropdown/removeDropdownOnblur';
-import search from './search/search';
-
-export default ()=>{
-    pollForEl(".search-input input").then(search)
-    removeDropdownOnblur()
-}
